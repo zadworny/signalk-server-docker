64d56af305032fcd077d44a572f58667aad526b2
KEGustafsson <ke.gustafsson@gmail.com>
2020-03-03 15:20:54 +0200

    Added run_me_1st.sh for init run

---
+++ run_me_1st.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker build -t signalk-server-node .
+docker run --name signalk-server-node --entrypoint /home/node/signalk/bin/signalk-server signalk-server-node &
+sleep 10
+docker cp signalk-server-node:/home/node/.signalk $PWD/.signalk
+docker stop signalk-server-node
+docker rm signalk-server-node
+
