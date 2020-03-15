0a7284fa57104359a14ac442298f4de07c5b320c
KEGustafsson <ke.gustafsson@gmail.com>
2020-03-15 19:19:06 +0200

    1st install script corrected

--- run_me_1st.sh
+++ run_me_1st.sh
@@ -2,7 +2,8 @@
 docker build -t signalk-server-node .
 docker run --name signalk-server-node --entrypoint /home/node/signalk/bin/signalk-server signalk-server-node &
 sleep 10
-docker cp signalk-server-node:/home/node/.signalk $PWD/../signalk_conf
+mkdir -p $PWD/../signalk_conf
+docker cp signalk-server-node:/home/node/.signalk/. $PWD/../signalk_conf
 docker stop signalk-server-node
 docker rm signalk-server-node
 
