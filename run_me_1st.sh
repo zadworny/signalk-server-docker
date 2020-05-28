11e2b83dbefca80712f0ccc3db7741d40b835082
KEGustafsson <ke.gustafsson@gmail.com>
2020-05-28 22:15:55 +0300

    Signalk, Influxdb and Grafana bundled

--- run_me_1st.sh
+++ run_me_1st.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 docker build -t signalk-server-node .
 docker run --name signalk-server-node --entrypoint /home/node/signalk/bin/signalk-server signalk-server-node &
+docker run --name grafana grafana/grafana &
+
 sleep 10
+
 mkdir -p $PWD/../signalk_conf
 docker cp signalk-server-node:/home/node/.signalk/. $PWD/../signalk_conf
 docker stop signalk-server-node
 docker rm signalk-server-node
+
+mkdir -p $PWD/../signalk_volume/influxdb
+mkdir -p $PWD/../signalk_volume/grafana/data
+mkdir -p $PWD/../signalk_volume/grafana/conf
+docker cp grafana:/var/lib/grafana/. $PWD/../signalk_volume/grafana/data
+docker cp grafana:/usr/share/grafana/conf/. $PWD/../signalk_volume/grafana/conf
+docker stop grafana
+docker rm grafana
+
 docker-compose up -d
